0fe0e9d904d1659ba5830ee2872bb2db6669c0b2
joaosoares <joaosoares@pal-nat186-84-175.itap.purdue.edu>
2025-04-22 09:05:26 -0400

    boom

---
+++ M3 - Algorithm Improvement/Final Files copy/M3_benchmark_011_03.m
@@ -0,0 +1,66 @@
+function [bench_comp, bench_sudan, bench_suv] = M3_benchmark_011_03(data)
+%M3_BENCHMARK_011_03 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+%Compact Hatchback benchmark parameters
+comp_ts = 6.21; %Acceleration start time (s)
+comp_tau = 1.51; %Time constant (s)
+comp_ispeed = -0.09; %Initial speed (m/s)
+comp_fspeed = 25.08; %Final speed (m/s)
+
+%Midsize Sudan benchmark parameters
+sudan_ts = 9.39; %Acceleration start time (s)
+sudan_tau = 1.96; %Time constant (s)
+sudan_ispeed = -0.22; %Initial speed (m/s)
+sudan_fspeed = 24.72; %Final speed (m/s)
+
+%SUV benchmark parameters
+suv_ts = 6.85; %Acceleration start time (s)
+suv_tau = 2.80; %Time constant (s)
+suv_ispeed = 0.19; %Initial speed (m/s)
+suv_fspeed = 24.18; %Final speed (m/s)
+
+
+%For loops
+
+%Control variable for loop, and input
+x_data = data(:,1);
+count_x = length(x_data);
+
+y_comp = [];
+y_sudan =[];
+y_suv = [];
+
+%For loop to calculate y-values for each of the cars
+for count = 1:count_x
+    %Compact Hatchback y-values
+    if(0 <= x_data(count) && x_data(count) < comp_ts)
+        y_comp = [y_comp, comp_ispeed];
+    elseif(x_data(count) >= comp_ts)
+        y_c = comp_ispeed + (comp_fspeed - comp_ispeed) * (1 - exp(-(x_data(count) - comp_ts) / (comp_tau)));
+        y_comp = [y_comp, y_c];
+    end
+    %Sudan y-values
+    if(0 <= x_data(count) && x_data(count) < sudan_ts)
+        y_sudan = [y_sudan, sudan_ispeed];
+    elseif(x_data(count) >= sudan_ts)
+        y_s = sudan_ispeed + (sudan_fspeed - sudan_ispeed) * (1 - exp(-(x_data(count) - sudan_ts) / (sudan_tau)));
+        y_sudan = [y_sudan, y_s];
+    end
+    %SUV y-values
+    if(0 <= x_data(count) && x_data(count) < suv_ts)
+        y_suv = [y_suv, suv_ispeed];
+    elseif(x_data(count) >= suv_ts)
+        y_sv = suv_ispeed + (suv_fspeed - suv_ispeed) * (1 - exp(-(x_data(count) - suv_ts) / (suv_tau)));
+        y_suv = [y_suv, y_sv];
+    end
+end
+
+%Outputs being assigned to the values created by the for loop.
+
+bench_comp = y_comp;
+bench_sudan = y_sudan;
+bench_suv = y_suv;
+end
+
